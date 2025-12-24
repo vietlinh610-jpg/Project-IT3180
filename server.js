21401592b03986989df05d0ab8a09757ca395db8
DaoQuocDai <daoquocdai2005@gmail.com>
2025-12-24 20:41:39 +0700

    Them quan ly tai khoan

--- server.js
+++ server.js
@@ -14,12 +14,14 @@ const canhoRoutes = require('./routes/canhoRoutes');
 const nhankhauRoutes = require('./routes/nhankhauRoutes');
 const tamvangRoutes = require('./routes/tamvangRoutes');
 const tamtruRoutes = require('./routes/tamtruRoutes');
+const taikhoanRoutes = require('./routes/taikhoanRoutes');
 // Sử dụng Routes
 app.use('/api/hokhau', hokhauRoutes);
 app.use('/api/canho', canhoRoutes);
 app.use('/api/nhankhau', nhankhauRoutes);
 app.use('/api/tamvang', tamvangRoutes);
 app.use('/api/tamtru', tamtruRoutes);
+app.use('/api/taikhoan', taikhoanRoutes);
 
 app.get('/', (req, res) => {
     res.send('API is ready!');
